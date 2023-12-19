48e4a1a8bac3b779d81040ea9f15e48d740ee03e
Thiago Zafalon Miranda <thiago.miranda@daedalus-tech.com>
2023-12-19 19:32:28 -0300

    Update mask conversion script

--- thiago/npy_to_cityscapes_mask.py
+++ thiago/npy_to_cityscapes_mask.py
@@ -6,7 +6,9 @@ import numpy as np
 
 def main() -> None:
     dir = Path("/home/thiago/Downloads/project-3-at-2023-12-19-21-51-72712f76")
-    a = np.load(dir / "task-1-annotation-2-by-2-tag-background-0.npy")
+    a = np.load(dir / "task-1-annotation-2-by-2-tag-road-0.npy")
+    a[np.nonzero(a)] = 1
+    assert np.all(np.unique(a) == np.asarray([0, 1]))
     cv2.imwrite("tmp.png", a)
 
 
